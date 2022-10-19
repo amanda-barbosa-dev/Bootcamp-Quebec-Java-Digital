a8e183dc5b27e0420f2670a84408915e594e79c7
Amanda <amandab.barbosa03@gmail.com>
2022-10-19 17:17:32 -0300

    Criação de uma classe para verificar os conceitos de condições encadeadas e condições ternárias encadeadas

---
+++ src/controledefluxos/ResultadoEscolar.java
@@ -0,0 +1,24 @@
+package controledefluxos;
+
+public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+
+        // Condicão encadeada (if, else if, else)
+        System.out.println("Resultado impresso com uma condição encadeada");
+        int notaFinal = 5;
+        if (notaFinal >=7) {
+            System.out.println("Aprovado");
+        } else if (notaFinal >=5 && notaFinal < 7) {
+            System.out.println("Recuperação");
+        } else {
+            System.out.println("Reprovado");
+        }
+
+         // Condicão ternária encadeada
+        System.out.println("Resultado impresso com uma condição ternária encadeada");
+        String resultadoAluno = notaFinal >= 7 ? "Aprovado" : notaFinal >=5 && notaFinal < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultadoAluno);
+    }
+
+}
