4e7c70eacdff73bccdc245e3a300f894e68c91f9
Amanda <amandab.barbosa03@gmail.com>
2022-10-19 11:21:23 -0300

    Criação de classe e método para entendimento do conceito de sobrecargas

---
+++ src/sobrecarga/Main.java
@@ -0,0 +1,13 @@
+package sobrecarga;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Quadrilatero.calcularArea(4);
+        Quadrilatero.calcularArea(4d,5d);
+        Quadrilatero.calcularArea(4,5,6);
+        Quadrilatero.calcularArea(4f,5f);
+
+        }
+    }
+
